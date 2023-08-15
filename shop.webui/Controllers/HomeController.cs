17a9d0d02815ef9b455dc232725482532f7fdbc7
Yağmur Yalçın <yagmuryalcin26@hotmail.com>
2023-08-15 14:38:42 +0300

    Add project files.

---
+++ shop.webui/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace shop.webui.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+            string mesaj = saat > 12 ? "İyi Günler" : "Günaydın";
+            ViewBag.Username = "Ahmet";
+            ViewBag.Greeting = mesaj;
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ shop.webui/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shop.webui.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult details()
+        {
+            return View();
+        }
+    }
+}
